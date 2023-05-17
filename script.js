b0a767c45f6252402854476458b1fa6ff09cb8a8
Merdione <88279017+Merdione@users.noreply.github.com>
2023-05-18 01:56:34 +0300

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+function git() {
+    window.location.href = "https://github.com/Merdione"
+}
+
+function steam() {
+    window.location.href = "https://steamcommunity.com/profiles/76561198987850620/"
+}
